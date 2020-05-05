7eb5e51f2fb18cd76a7a5b3dd376ee700a041025
M <bmaicel@yahoo.com>
2020-05-05 19:49:20 +0300

    tests added

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,24 +1,26 @@
-import {RegionComponent} from './region.component';
+import {RegionComponent} from '../region/region.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {EmployeesComponent} from '../employees/employees.component';
 import {HomeComponent} from '../home/home.component';
-import {LoginComponent} from '../login/login.component';
+import {LoginComponent} from './login.component';
 import {ProfileComponent} from '../profile/profile.component';
 import {EmployeesSkillProfileComponent} from '../employees-skill-profile/employees-skill-profile.component';
 import {ProjectsComponent} from '../projects/projects.component';
 import {MasterdataComponent} from '../master-data/masterdata.component';
 import {RegisterComponent} from '../register/register.component';
 import {AppRoutingModule} from '../app-routing.module';
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ToastrModule} from 'ngx-toastr';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {LoginService} from '../services/login.service';
+import {CookieService} from 'ngx-cookie-service';
 
-describe('RegionComponent', () => {
-  let component: RegionComponent;
-  let fixture: ComponentFixture<RegionComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
   let httpMock: HttpTestingController;
 
   beforeEach(async(() => {
@@ -43,14 +45,14 @@ describe('RegionComponent', () => {
         BrowserAnimationsModule,
         ToastrModule.forRoot(),
       ],
-      providers: [],
+      providers: [ LoginService, CookieService ],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegionComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     httpMock = TestBed.get(HttpTestingController);
     fixture.detectChanges();
@@ -61,21 +63,20 @@ describe('RegionComponent', () => {
   });
 
   it('should render the correct title', () => {
-    const title = fixture.nativeElement.querySelectorAll('h1')[0];
-    expect(title.innerText).toBe('Manage Regions');
+    const compile = fixture.nativeElement;
+    expect(compile.querySelectorAll('h3')[0].innerText).toBe('Proiect Colectiv');
   });
 
-  it('should render correct buttons', () => {
-    const divs = fixture.nativeElement.querySelectorAll('div');
-    expect(divs.length).toBe(2);
-    expect(divs[1].querySelectorAll('button').length).toBe(2);
+  it('should generate only two input fields, one for username and one for password', () => {
+    const compile = fixture.nativeElement;
+    expect(compile.querySelectorAll('input').length).toBe(2);
+    expect(compile.querySelectorAll('input#username').length).toBe(1);
+    expect(compile.querySelectorAll('input#password').length).toBe(1);
   });
 
-  it('should fire the save() method when save button is clicked', async(() => {
-    const save = spyOn(component, 'save');
-    const saveBtn = fixture.nativeElement.querySelectorAll('button')[1];
-    saveBtn.triggerEventHandler('click', null);
-
-    expect(save).toHaveBeenCalled();
-  }));
+  it('should generate a button with Login text', () => {
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toBe(1);
+    expect(button[0].innerText).toBe('Login');
+  });
 });
